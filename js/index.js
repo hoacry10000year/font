e589bb6ae40ecf6396460836575ff4e9a4b4da1b
hoacry10000year <162078055+hoacry10000year@users.noreply.github.com>
2025-05-23 11:05:09 +0700

    data type of js

---
+++ js/index.js
@@ -0,0 +1,34 @@
+/* inline scrip
+   externa
+   internal
+   
+   Variables are containers of data
+   Have 3 choice declare variable : var, let , const
+   let is use when  variable change in differnt time 
+   const is use when variable not chang in differnt time
+*/
+// comment can make code reable
+
+// Welcome to 30DaysOfjavascript
+
+/*
+    comment can make code reable, easy ro reuse and informative
+*/
+
+//  Types of variables
+
+// String variable
+let firstName = 'Nhat Hoa';
+
+// Boolean variable
+let isStudent = 'false';
+
+// Underfined variable
+let lastName;
+
+// Null variable;
+let age = null;
+
+// Number variable
+let year = 2023;
+
